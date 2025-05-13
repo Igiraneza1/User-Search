bc9986c160064fb1fd042e07a6e123f7801dc672
Igiraneza1 <igiranezaadeline@gmail.com>
2025-05-13 10:14:45 +0300

    commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,8 @@
 export default function Home() {
   return (
-    <div>
-      <h1 className="text-2xl font-bold">Welcome to the Home Page</h1>
-      <p className="mt-4">Navigate to the Users page to see user details.</p>
+    <div className="flex flex-col justify-center items-center p-5  bg-amber-500  rounded-b-xl ">
+      <h1 className="text-2xl font-bold p-5">Welcome to the Home Page</h1>
+      <p className="">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Numquam laudantium nam accusamus voluptate? Numquam sunt id iure omnis? Autem deserunt dolorem nisi assumenda, recusandae cumque exercitationem distinctio dignissimos voluptate ea?</p>
     </div>
   );
 }
--- src/component/navbar.jsx
+++ src/component/navbar.jsx
@@ -3,9 +3,8 @@ import Link from "next/link";
 
 export default function Navbar() {
   return (
-    <nav className="bg-blue-700 text-white py-4 shadow-md">
-      <div className="container mx-auto px-4 flex items-center justify-between">
-        <h1 className="text-xl font-semibold tracking-wide">MyApp</h1>
+    <nav className="bg-blue-900 text-white py-4 shadow-md">
+      <div className="container mx-auto px-4 flex items-center justify-center ">
         <div className="flex gap-6">
           <Link href="/" className="hover:text-blue-200 transition">Home</Link>
           <Link href="/users/1" className="hover:text-blue-200 transition">User 1</Link>
