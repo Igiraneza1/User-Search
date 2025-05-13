e2c63fd3f1f6fcf7bd2066e9274eb93f49031db3
Igiraneza1 <igiranezaadeline@gmail.com>
2025-05-13 10:16:21 +0300

    commit

--- src/component/navbar.jsx
+++ src/component/navbar.jsx
@@ -7,9 +7,9 @@ export default function Navbar() {
       <div className="container mx-auto px-4 flex items-center justify-center ">
         <div className="flex gap-6">
           <Link href="/" className="hover:text-blue-200 transition">Home</Link>
-          <Link href="/users/1" className="hover:text-blue-200 transition">User 1</Link>
-          <Link href="/users/2" className="hover:text-blue-200 transition">User 2</Link>
-          <Link href="/users/3" className="hover:text-blue-200 transition">User 3</Link>
+          <Link href="/users/1" className="hover:text-blue-300 transition">User 1</Link>
+          <Link href="/users/2" className="hover:text-blue-300 transition">User 2</Link>
+          <Link href="/users/3" className="hover:text-blue-300 transition">User 3</Link>
         </div>
       </div>
     </nav>
